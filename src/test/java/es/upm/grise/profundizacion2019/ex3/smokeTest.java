4c97c8dbdd658d8b62b2d0dc85b6df35504bcb99
JMonjo <55985859+JMonjo@users.noreply.github.com>
2020-01-24 16:35:31 +0100

    pruebas

--- src/test/java/es/upm/grise/profundizacion2019/ex3/smokeTest.java
+++ src/test/java/es/upm/grise/profundizacion2019/ex3/smokeTest.java
@@ -2,20 +2,35 @@ package es.upm.grise.profundizacion2019.ex3;
 
 import static org.junit.Assert.*;
 
+import static org.mockito.Mockito.*;
+
+import java.time.LocalDateTime;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.*;
 
 public class smokeTest {
-	MyClass my;
 	
-	@Before
-	public void setUp() {
-		my = new MyClass();
-	}
-
+	// Prueba anaidir 120segundos a la fecha
 	@Test
-	public void smoleTest() {
-		long SECONDS = 60;
-		my.nextTime(SECONDS);
+	public void fechaMasCientoVeinteSegundos() {
+		final LocalDateTime localDateTime = LocalDateTime.of(2020,01,01,00,00,00);
+		final long seconds = 120;
+		//final LocalDateTime localDateTimeexpected = LocalDateTime.of(2020,01,01,00,02,00);
+		final String localDateTimeExpected = "2020/01/01 00:02:00";
+		final Time time = new Time(localDateTime);
+		assertEquals(localDateTimeExpected, time.getFutureTime(seconds);
+	}
+	
+	
+	// Prueba numero de invocaciones getFutureTime
+	@Test
+	public void unaInvocacionFutureTime() {
+		final Time mock = mock(Time.class);
+		final MyClass myClass = new MyClass(mock);
+		final long seconds = 60;
+		myClass.nextTime(seconds);
+		verify(time, times(1)).getFutureTime(seconds);
 	}
 }
